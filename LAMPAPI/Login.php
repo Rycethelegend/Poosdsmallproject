e6168b919a20779dbb70823774d21360e8c02d92
Jake Arnold <jakemarnold17@gmail.com>
2026-09-11 02:22:59 -0400

    Outline of Login w Comments

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,3 +1,16 @@
 <?php
 
+    #get input data from request
+
+
+    #Attempt to connect to database
+
+    
+        #attempt to find user in database and login, return Json
+
+
+    #if not found / no data found, return error with Json
+
+
+
 ?>
